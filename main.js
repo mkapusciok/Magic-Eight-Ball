282fd2ac74fb0a1eca251a6f2af5b9ed6c4fd90b
mkapusciok <m.kapusciok@interia.pl>
2022-12-24 14:41:47 +0100

    Create randomNumber variable

--- main.js
+++ main.js
@@ -2,3 +2,4 @@ const userName = '';
 userName ? console.log('Hello, ${username}!') : console.log('Hello!');
 const userQuestion = 'Type your question here';
 console.log(userQuestion);
+log randomNumber = Math.floor(Math.random() * 8);
