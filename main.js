5b06cecddb03547965e843e3a30880984b8a2a1b
mkapusciok <m.kapusciok@interia.pl>
2022-12-24 14:56:31 +0100

    Fixed error

--- main.js
+++ main.js
@@ -1,33 +1,42 @@
-const userName = '';
+let userName = 'Madzia';
+
 userName ? console.log('Hello, ${username}!') : console.log('Hello!');
-const userQuestion = 'Type your question here';
-console.log(userQuestion);
+
+let userQuestion = 'Will I adopt a new cat?';
+
+console.log(`${userName} has asked - ${userQuestion}`);
+
 let randomNumber = Math.floor(Math.random() * 8);
+
+console.log(randomNumber);
+
 let eightBall = '';
+
 switch(randomNumber) {
-    case '0':
+    case 0:
         eightBall = 'It is certain';
         break;
-    case '1':
+    case 1:
         eightBall = 'It is decidedly so';
         break;
-    case '2':
+    case 2:
         eightBall = 'Reply hazy try again';
         break;
-    case '3':
+    case 3:
         eightBall = 'Cannot predict now';
         break;
-    case '4':
+    case 4:
         eightBall = 'Do not count on it';
         break;
-    case '5':
+    case 5:
         eightBall = 'My sources say no';
         break;
-    case '6':
+    case 6:
         eightBall = 'Outlook not so good';
         break;
-    case '7':
+    case 7:
         eightBall = 'Signs point to yes';
         break;
 }
+
 console.log(eightBall);
